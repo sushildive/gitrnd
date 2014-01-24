a88116eff36909e43899786fe9faf82c50cf2651
Sushil Dive <sushilchandra.dive@nttdata.com>
2014-01-24 10:23:46 +0530

    Fixed compilation issue

--- cxfwsrnd/ws-client/src/main/java/co/sg/client/Bootstrap.java
+++ cxfwsrnd/ws-client/src/main/java/co/sg/client/Bootstrap.java
@@ -1,13 +1,13 @@
 package co.sg.client;
 
 import co.sg.basicmath.basicmath.DividePortType;
-import co.sg.basicmath.basicmath.MathOperationsServiceSOAP;
-import co.sg.basicmath.basicmath_types.MathOperands;
+import co.sg.basicmath.basicmath.DividePortTypeImplService;
+import co.sg.basicmath.basicmath.types.MathOperands;
 
 public class Bootstrap {
 	public static void main(String[] args) {
-		MathOperationsServiceSOAP mathOperationsServiceSOAP = new MathOperationsServiceSOAP();
-		DividePortType dividePortType = mathOperationsServiceSOAP.getMathOperationPortSOAP();
+		DividePortTypeImplService mathOperationsServiceSOAP = new DividePortTypeImplService();
+		DividePortType dividePortType = mathOperationsServiceSOAP.getDividePortTypePort();
 		MathOperands mo = new MathOperands();
 		mo.setNumber1(1);
 		mo.setNumber2(2);
